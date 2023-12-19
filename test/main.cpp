fc1d5094679093e928e33864a1c55f1a84f08f45
Chealemon <1160216662@qq.com>
2023-12-19 17:50:18 +0800

    configure for MVS development&untrack cache

--- test/main.cpp
+++ test/main.cpp
@@ -1,19 +1,81 @@
 #include <iostream>
+#include "MvCameraControl.h"
 #include "ClientSocket.h"
 
 int main(int argc, char** argv)
 {
-	const std::string serverAddr("192.168.1.12");
-	ClientSocket client(serverAddr, 800);
+    /*初始化运行环境*/
+    
+    void* handle;
+    MV_CC_DEVICE_INFO_LIST stDeviceList;
+    memset(&stDeviceList, 0, sizeof(MV_CC_DEVICE_INFO_LIST));
+    int nRet = MV_CC_EnumDevices(MV_USB_DEVICE, &stDeviceList);
+    if (MV_OK != nRet)
+    {
+        printf("Enum Devices fail! nRet [0x%x]\n", nRet);
+        return -1;
+    }
 
-	std::string buffer;
-	std::cout << "send:";
-	while (buffer != std::string("q"))
-	{
-		std::getline(std::cin, buffer);
-		std::cout << buffer.size();
-		client.Send(buffer, buffer.size());
-	}
+    if (stDeviceList.nDeviceNum > 0)
+    {
+        for (unsigned int i = 0; i < stDeviceList.nDeviceNum; i++)
+        {
+            printf("[device %d]:\n", i);
+            MV_CC_DEVICE_INFO* pDeviceInfo = stDeviceList.pDeviceInfo[i];
+            if (NULL == pDeviceInfo)
+            {
+                return -1;
+            }
+        }
+    }
+    else
+    {
+        printf("Find No Devices!\n");
+        return -1;
+    }
+
+    //printf("Please Input camera index:");
+    unsigned int nIndex = 0;
+    //scanf_s("%d", &nIndex);
+
+    if (nIndex >= stDeviceList.nDeviceNum)
+    {
+        printf("Input error!\n");
+        return -1;
+    }
+
+    // Select device and create handle
+    nRet = MV_CC_CreateHandle(&handle, stDeviceList.pDeviceInfo[nIndex]);
+    if (MV_OK != nRet)
+    {
+        printf("Create Handle fail! nRet [0x%x]\n", nRet);
+        return -1;
+    }
+
+    // Open the specified device
+    nRet = MV_CC_OpenDevice(handle);
+    if (MV_OK != nRet)
+    {
+        printf("Open Device fail! nRet [0x%x]\n", nRet);
+        return -1;
+    }
+
+    // Close device
+    nRet = MV_CC_CloseDevice(handle);
+    if (MV_OK != nRet)
+    {
+        printf("ClosDevice fail! nRet [0x%x]\n", nRet);
+        return -1;
+    }
+
+    // Destroy handle
+    nRet = MV_CC_DestroyHandle(handle);
+    if (MV_OK != nRet)
+    {
+        printf("Destroy Handle fail! nRet [0x%x]\n", nRet);
+        return -1;
+    }
+    printf("Device successfully closed.\n");
 
 	return 0;
 }
